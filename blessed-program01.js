58cb7494d795135e28c81ec4338353bc60fdf29b
danyxudong <50774334+danyxudong@users.noreply.github.com>
2019-12-12 10:46:22 +0800

    My blessed.program() 1st commit

---
+++ blessed-program01.js
@@ -0,0 +1,22 @@
+// jshint esversion: 8
+
+/*
+  My Example for Blessed
+*/
+
+const blessed = require("blessed");
+const program = blessed.program();
+
+process.title = "My Example of Blessed.program()";
+
+program.on("keypress", function(ch, key) {
+  if (key.name === "q") {
+    process.exit(0);
+  }
+});
+
+program.enableMouse();
+
+program.move(1, 1);
+program.bg("green");
+program.write("Hello world", "red fg");
